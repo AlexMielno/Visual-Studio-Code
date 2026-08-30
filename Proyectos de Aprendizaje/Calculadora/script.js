23adb9bf8e10ffc61b1ef4fc7da4a8f40cbf4c68
Mielno <mileno2533@gmail.com>
2026-08-30 17:07:09 -0400

    Ahora se paga el objeto boton a digito()

--- Proyectos de Aprendizaje/Calculadora/script.js
+++ Proyectos de Aprendizaje/Calculadora/script.js
@@ -1,7 +1,7 @@
 function digito(boton){
 const escribir=document.getElementById("texto");
-const boton=document.querySelector("boton")
-const seleccion=boton.querySelectorAll("button");
-escribir.value+=seleccion[0].innerHTML;
+
+
+escribir.value+=boton.innerHTML;
 
 }
